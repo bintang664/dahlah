494a4fbebbabbd465e530996bcb782ef12d8a878
bintang664 <77222386+bintang664@users.noreply.github.com>
2021-01-11 06:46:52 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const  XBOT = 'AMPIBI'; // Nama Bot Whatsapp
-const instagram = 'https://instagram.com/affis_saputro123'; // Nama Instagramlu cok
-const nomer = 'https://Wa.me/+6282334297175'; // Nomor whatsapplu cok
+const  XBOT = 'Bintang664'; // Nama Bot Whatsapp
+const instagram = 'https://instagram.com/bintang.775'; // Nama Instagramlu cok
+const nomer = 'https://Wa.me/+6281351845561'; // Nomor whatsapplu cok
 const aktif = 'Tergantung kuota'; // Kapan bot lu aktif
 const groupwa = 'comming soon'; // OFFICIAL GRUP LU 1
-const youtube = 'https://youtube.com/channel/UCYKxsg7iF9a9IZyXQRNsvqw'; 
+const youtube = 'Gak punya'; 
 const qrcode = require("qrcode-terminal");
 const moment = require("moment");
 const cheerio = require("cheerio");
